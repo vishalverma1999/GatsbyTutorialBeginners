8e5946d434a4036d3137a8d66db5191584436f29
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-02 14:46:20 +0530

    Gatsby Tutorial #9 - Site Metadata & Page Queries

--- web-warrior/gatsby-config.js
+++ web-warrior/gatsby-config.js
@@ -7,4 +7,16 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+
+  // gatsby is automatically going to take metadata and add it to the graphql layer and no plugins or anything needed
+  siteMetadata: {   
+    // we can have different properties and values inside this object
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright @2022 Web Warrior',
+  }
 }
+
+/*
+every time u change gatsby config file u need to restart gatsby
+ */
