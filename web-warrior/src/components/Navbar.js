27da0ca7ded7bab2b321e25c7b0aa02b852fd7ab
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-02 15:59:35 +0530

    Gatsby Tutorial #10 - Static Queries (useStaticQuery)

--- web-warrior/src/components/Navbar.js
+++ web-warrior/src/components/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 
 export default function Navbar() {
+
+    // how to make a query inside a component that is not a page we can't do it the same way as we done in pages. instead what we have to do is use something called a static query using the hook called useStaticQuery and this is the hook we use to make a query inside the component this time we don't do it outside the component unlike in pages we have done
+    // we can remove- query SiteInfo and it will work perfectly ok
+    // here is a difference between these static queries right here and the page queries at the bottom of page components and that is in page components we can use query variables now we've not seen those yet but you'll see them later and in these queries you can't use query variables so just be aware of that 
+    // one other thing i also want to mention is that this hook right here sometimes it can be a bit temperamental and throw an error one thing i've noticed is that if i use components that are not capitalized right here this sometimes throws an error and it doesn't really like it so i always make sure these are capitalized so i don't get that error now i'm not done much digging around with this it's just my recent experience using it
+    // finally one more thing you can only use this hook once inside a component so you can't use this hook again down here as that won't work 
+
+    const data = useStaticQuery(graphql`
+    query SiteInfo {    
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `)
+
+    const { title } = data.site.siteMetadata;
+
     return (
         <nav>
-            <h1>Web Warrior</h1>
+            <h1>{title}</h1>
             <div className='links'>
                 <Link to='/' >Home</Link>
                 <Link to='/about'>About</Link>
@@ -17,3 +36,4 @@ export default function Navbar() {
 /*
 Instead,it would be nice to create a single layout component which incorporates the navbar the footer and anything else that's consistent across all pages and then just wrap all of our different pages right here with that single layout component 
 */
+
--- web-warrior/src/pages/index.js
+++ web-warrior/src/pages/index.js
@@ -1,13 +1,11 @@
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
 
 
-export default function Home({ data }) {
+export default function Home() {
 
-  console.log(data);
-  const { description, title } = data.site.siteMetadata;
   return (
     <Layout>
       {/* content b/w layout wrapper is children of the wrapper and we can collect it in Layout component as prop from each page */}
@@ -19,25 +17,10 @@ export default function Home({ data }) {
           <Link className={styles.btn} to='/project'>My Portfolio Projects</Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        <p> {title} - {description} </p>
       </section>
     </Layout>
   )
 }
 
-// How do we actually make that query from our components well there's a couple of different ways that we can make the queries from our components in this lesson i'm going to show you how to make page queries from our page components and then later on i'll show you how to make queries from other components that are not pages as well 
-export const query = graphql`
-query SiteInfo {
-  site {
-    siteMetadata {
-      description
-      title
-    }
-  }
-}
-`
-// how do we actually use this data inside our component well gatsby is going to find this query first of all and then it's going to give us access to a data property inside our component on the props so we automatically take in props right here this props object right but what we could do is just destructure from that object a data property and that is any data that we get back from this thing right here from this query
 
 
-
- 
